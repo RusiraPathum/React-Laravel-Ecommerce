31dc04849c0f560457a969db5cd45231f3b9447b
RusiraPathum <rusira.pathum20@gmail.com>
2022-02-15 23:09:19 +0530

    registration part done

--- frontend/src/components/frontend/auth/Register.js
+++ frontend/src/components/frontend/auth/Register.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
 import axios from 'axios';
+import Swal from 'sweetalert2'
+
 import Navbar from "../../../layouts/frontend/Navbar";
+import { useHistory } from "react-router-dom";
 
 const Register = () => {
+
+    const history = useHistory();
 
     const [registerInput, setRegister] = useState({
         name: '',
         email: '',
         password: '',
+        error_list: [],
     });
 
     const handleInput = (e) => {
-        e.presist();
-        setRegister({...registerInput, [e.target.name]: e.target.value});
+        e.persist();
+        setRegister({ ...registerInput, [e.target.name]: e.target.value });
     }
 
     const registerSubmit = (e) => {
@@ -24,16 +30,40 @@ const Register = () => {
             password: registerInput.password,
         }
 
-        axios.post('http://127.0.0.1:8000/api/register', data).then(res => {
+        axios.get('/sanctum/csrf-cookie').then(response => {
+
+            axios.post('/api/register', data).then(res => {
+                
+                if(res.data.status === 200){
+
+                    localStorage.setItem('auth_tokn', res.data.token);
+                    localStorage.setItem('auth_name', res.data.username);
+
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: res.data.message,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                    history.push('/');
+
+                }else{
+
+                    setRegister({...registerInput, error_list: res.data.validation_error});
+
+                }
+
+            });
 
         });
 
     }
 
-    return ( 
+    return (
         <div>
-            <Navbar/>
-            
+            <Navbar />
+
             <div className="container py-5">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
@@ -46,14 +76,17 @@ const Register = () => {
                                     <div className="form-group mb-3">
                                         <label>Full Name</label>
                                         <input type="text" name="name" className="form-control" onChange={handleInput} value={registerInput.name} />
+                                        <span>{registerInput.error_list.name}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <label>Email</label>
                                         <input type="email" name="email" className="form-control" onChange={handleInput} value={registerInput.email} />
+                                        <span>{registerInput.error_list.email}</span>
                                     </div>
                                     <div className="form-group mb-3">
                                         <label>Password</label>
                                         <input type="password" name="password" className="form-control" onChange={handleInput} value={registerInput.password} />
+                                        <span>{registerInput.error_list.password}</span>
                                     </div>
                                     <div className="form-group d-flex  justify-content-end mb-3">
                                         <button type="submit" className="btn btn-primary" value="">Register</button>
@@ -67,7 +100,7 @@ const Register = () => {
             </div>
 
         </div>
-     );
+    );
 }
- 
+
 export default Register;
